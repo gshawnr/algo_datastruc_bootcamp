b6a7073f568bba50cf344e8381a16927e0579dd7
gsr <gshawnr@gmail.com>
2022-07-12 15:07:41 -0700

    finish anagram and capitalize

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,35 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// NOTES:
+// Should handle the "Starting case" first with your push - ie chunked = 0 should be the if not the else
+
+function chunk(array, size) {
+  // SOLUTION 1
+  //   let chunked = [];
+  //   let chunkedIndex = 0;
+  //   let currChunkLength = 0;
+  //   for (let i = 0; i < array.length; i++) {
+  //     if (currChunkLength !== 0 && currChunkLength < size) {
+  //       chunked[chunkedIndex].push(array[i]);
+  //       currChunkLength = chunked[chunkedIndex].length;
+  //     } else {
+  //       chunked.push([array[i]]);
+  //       chunkedIndex = chunked.length - 1;
+  //       currChunkLength = chunked[chunkedIndex].length;
+  //     }
+  //   }
+  //   return chunked;
+
+  // SOLUTION 2
+  let chunked = [];
+  let start = 0;
+
+  while (start <= array.length - 1) {
+    chunked.push(array.slice(start, start + size));
+    start = start + size;
+  }
+  return chunked;
+}
 
 module.exports = chunk;
