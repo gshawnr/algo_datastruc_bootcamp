600e9347039017d0e23b792ef80c8ff9d209e7ec
Shawn Richardson <gshawnr@gmail.com>
2023-06-29 08:13:53 -0700

    remove completed maxints work

--- exercises/maxints/index.js
+++ exercises/maxints/index.js
@@ -1,43 +1,6 @@
 // write a function to retrive the sum of the two largest integers from an input array
 // use the more scalable method (ie not array sort)
 
-function maxInts(arr) {
-  if (arr.length < 2) return null;
+function maxInts(arr) {}
 
-  let maxInts = [];
-  let maxIndexes = [];
-
-  for (let i = 0; i < 2; i++) {
-    let { max, index } = getMax(arr, maxIndexes);
-    maxInts.push(max);
-    maxIndexes.push(index);
-  }
-
-  let sum = null;
-  for (let val of maxInts) {
-    sum += val;
-  }
-  return sum;
-}
-
-function getMax(arr, indexesToExclude) {
-  let maxInt;
-  let maxIndex;
-
-  for (let i = 0; i < arr.length; i++) {
-    let val = arr[i];
-
-    if (!maxInt) {
-      maxInt = val;
-      maxIndex = i;
-    }
-
-    if (val > maxInt && !indexesToExclude.includes(i)) {
-      maxInt = val;
-      maxIndex = i;
-    }
-  }
-
-  return { max: maxInt, index: maxIndex };
-}
 module.exports = maxInts;
