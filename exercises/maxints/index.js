0a7470a8647debf5b31359e6110a1d4c983c9890
Shawn Richardson <gshawnr@gmail.com>
2023-07-01 11:15:52 -0700

    add maxints to order

--- exercises/maxints/index.js
+++ exercises/maxints/index.js
@@ -1,5 +1,6 @@
 // write a function to retrive the sum of the two largest integers from an input array
 // use the more scalable method (ie not array sort)
+// return null if 2 numbers are not provided
 
 function maxInts(arr) {}
 
