cb77fefec9d3c42a77eeef1db71126ce929929cb
Shawn Richardson <gshawnr@gmail.com>
2025-08-07 09:19:18 -0400

    update grouptransactions test

--- exercises/grouptransactions/index.js
+++ exercises/grouptransactions/index.js
@@ -1,3 +1,12 @@
+/*
+  Complete the function to take an input array of strings (transactions) and ouput an ordered array that groups transactions and orders them first by the number of transactions
+  secondly by the transaction
+
+  ie 
+  input: ["logoff", "text", "logon", "vim", "logon", "logoff"]
+  output: ["logoff 2", "logon 2", "text 1", "vim 1"]
+
+*/
 function groupTransactions(transactions) {}
 
 module.exports = groupTransactions;
