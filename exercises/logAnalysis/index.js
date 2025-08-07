362a48750ace5e9ce1c33f201b5d1ff6b2b4fe9e
Shawn Richardson <gshawnr@gmail.com>
2025-08-07 11:12:22 -0400

    fix function declaration

--- exercises/logAnalysis/index.js
+++ exercises/logAnalysis/index.js
@@ -7,6 +7,9 @@ timestamp represents the time in seconds since the application was last launched
 action will be either "sign-in" or "sign-out"
 Given a log with entries in no particular order, return an array of strings that denote user_id's of 
 users who signed out in maxSpan seconds or less after signing in. 
+
+Return: a string array of user id's, sorted ascending by numeric value
 */
 
 function logAnalysis(logs, maxSpan) {}
+module.exports = logAnalysis;
